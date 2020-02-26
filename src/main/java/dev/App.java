fdf01f74044426d7c45cd512b72c3aecdf89b4fa
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-02-26 16:13:05 +0100

    TP01 : maven : étape finale!

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
 /**
  * Hello world!
  *
@@ -8,6 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	  String asciiArt = FigletFont.convertOneLine("App");
+    	    System.out.println(asciiArt);
     }
 }
